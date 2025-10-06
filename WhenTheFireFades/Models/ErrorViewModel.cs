000df3853bb36e037e166d547ac73a771da53ec4
Dennis Molund <molund1994@gmail.com>
2025-10-06 22:03:37 +0700

    Add project files.

---
+++ WhenTheFireFades/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhenTheFireFades.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WhenTheFireFades/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+     throw new InvalidOperationException("Connection string 'DatabaseConnectionString'" +
+    " not found.");
+
+builder.Services.AddTransient<SqlConnection>(sp =>
+    new SqlConnection(sp.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection")));
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
